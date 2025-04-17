1d37ee09b243029e07cf3b60c7f09cdfb56ff402
Abusolay <sefat01625@gmail.com>
2025-04-18 01:09:46 +0600

    foote section

--- book-poka/src/Routes/Routes.jsx
+++ book-poka/src/Routes/Routes.jsx
@@ -5,11 +5,20 @@ import {
   } from "react-router";
 import Root from '../Pages/Root/Root/Root';
 import ErrorPage from '../Pages/Root/ErrorPage/ErrorPage';
+import Home from '../Pages/Root/Home/Home';
   
   export const router = createBrowserRouter([
     {
       path: "/",
       Component: Root,
-      errorElement: <ErrorPage></ErrorPage>
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+            index: true,
+            path: "/",
+            Component: Home
+        }
+      ]
+      
     },
   ]);
